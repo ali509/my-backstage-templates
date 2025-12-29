11f0c2050e2bda403df75c521565ef4ce2761107
Hussen Ali <39634565+ali509@users.noreply.github.com>
2025-12-29 18:38:30 +0530

    Update index.js

--- skeleton/index.js
+++ skeleton/index.js
@@ -1,1 +1,8 @@
+/**
+ * Project: ${{ values.name }}
+ * Owner: ${{ values.owner }}
+ */
 
+console.log("Hello, World!");
+console.log("This component was scaffolded by Backstage.");
+console.log("Repository Location: ${{ values.repoUrl }}");
